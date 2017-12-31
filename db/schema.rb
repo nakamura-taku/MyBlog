22727b633e14efee7cfa690c79cadf865a050add
Taku <taku.nakamura0904@gamil.com>
2017-12-31 16:24:44 +0900

    Add price column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228132358) do
+ActiveRecord::Schema.define(version: 20171231062135) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171228132358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "price"
   end
 
 end
